5895b786ddd1655b3ad1550e8da3ffbee168f12f
Ahmet Kürşad YILDIZ <ahmetkursadyildiz@gmail.com>
2025-06-21 00:46:20 +0300

    Some of the entities were written, those related to customers

---
+++ CRM.Domain/Entities/Activity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM.Domain.Entities
+{
+    public class Activity
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public Guid CustomerId { get; set; }
+        public string Type { get; set; } = string.Empty; // Call, Email, Meeting, etc.
+        public string Note { get; set; } = string.Empty;
+        public DateTime Date { get; set; } = DateTime.UtcNow;
+
+        // Navigation Property
+        public Customer? Customer { get; set; }
+    }
+}
---
+++ CRM.Domain/Entities/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM.Domain.Entities
+{
+
+    public class Customer
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public string CompanyName { get; set; } = string.Empty;
+        public string ContactPerson { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+
+        // Navigation Property
+        public ICollection<Activity> Activities { get; set; } = new List<Activity>();
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
