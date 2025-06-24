a947550db09f422ff7061f98b636edca050bb823
Ahmet Kürşad YILDIZ <ahmetkursadyildiz@gmail.com>
2025-06-24 13:13:57 +0300

    Automapper library added, needs to be examined service layer added, CRUD operations will be written

---
+++ CrmApplication/CrmApplication/Profiles/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using CrmApplication.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrmApplication.NewFolder
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Customer,CustomerDto>
+        }
+    }
+}
---
+++ CrmApplication/CrmInfrastructure/Services/CustomerService.cs
@@ -0,0 +1,56 @@
+﻿using CrmApplication.DTOs;
+using CrmApplication.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM.Infrastructure.Services
+{
+    public class CustomerService : ICustomerService
+    {
+        private readonly AppDbContext _context;
+
+        public CustomerService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<CustomerDto> CreateAsync(CustomerCreateDto customerCreateDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<CustomerDto>> GetAllAsync()
+        {
+            return await _context.Customers
+                .Select(c => new CustomerDto
+                {
+                    Id = c.Id,
+                    CompanyName = c.CompanyName,
+                    ContactPerson = c.ContactPerson,
+                    Email = c.Email,
+                    Phone = c.Phone,
+                    Address = c.Address
+                })
+                .ToListAsync();
+        }
+
+        public async Task<CustomerDto> GetByIdAsync(Guid id)
+        {
+            var customer = await _context.Customers.FindAsync(id);
+            return customer is null ? null : MapToDto(customer);
+        }
+
+        public Task<bool> UpdateAsync(CustomerUpdateDto customerUpdateDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
